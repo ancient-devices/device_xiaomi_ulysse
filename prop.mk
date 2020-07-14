a222bb370fefbc8af8031a1e7144e0355667ff35
zqm64 <zqm64@outlook.com>
2020-07-14 19:22:56 +0800

    ugg: Commonize some system props

--- prop.mk
+++ prop.mk
@@ -1,15 +1,3 @@
-# Audio
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.audio.fluence.speaker=true \
-persist.vendor.audio.fluence.voicecall=true \
-persist.vendor.audio.fluence.voicerec=false \
-ro.vendor.audio.sdk.fluencetype=fluence
-
-# Camera
-PRODUCT_PROPERTY_OVERRIDES += \
-vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
-persist.camera.dual.camera=0
-
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.qfp=false
