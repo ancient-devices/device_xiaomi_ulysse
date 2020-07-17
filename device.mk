f69a72c3020b82218727a85dfeb2d153fa81162f
zqm64 <zqm64@outlook.com>
2020-07-17 18:57:28 +0800

    ugg: Commonize Camera

--- device.mk
+++ device.mk
@@ -37,11 +37,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
 
-# Camera
-PRODUCT_PACKAGES += \
-    camera.msm8937 \
-    libmm-qcamera
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1 \
