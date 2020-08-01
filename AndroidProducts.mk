b10e6450a606ebc826ce4b177afa4fa234c106b0
Insan23 <green.blaze235@gmail.com>
2020-08-01 13:38:07 +0700

    ROMBRINGUP: ugg: AospExtended Bring Up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,10 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_ugg.mk \
-    $(LOCAL_DIR)/ancient_ugg.mk
+    $(LOCAL_DIR)/ancient_ugg.mk \
+    $(LOCAL_DIR)/aosp_ugg.mk
+    
+COMMON_LUNCH_CHOICES := \
+    aosp_ugg-user \
+    aosp_ugg-userdebug \
+    aosp_ugg-eng
