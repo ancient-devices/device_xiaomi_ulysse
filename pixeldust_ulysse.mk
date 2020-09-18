8f70cc3681106e6a8d94cd1987d645ba283e0452
Insan23 <green.blaze235@gmail.com>
2020-09-18 14:57:05 +0700

    ROMBRINGUP: ulysse: PixelDust Bringup!

---
+++ pixeldust_ulysse.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from mido device
+$(call inherit-product, device/xiaomi/ulysse/device.mk)
+
+# Inherit some common PixelDust CAF stuff.
+$(call inherit-product, vendor/pixeldust/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := ulysse
+PRODUCT_NAME := pixeldust_ulysse
+BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 5A
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="ugg-user 7.1.2 N2G47H V9.5.8.0.NDKMIFA release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "xiaomi/ugg/ugg:7.1.2/N2G47H/V9.5.8.0.NDKMIFA:user/release-keys"
+
+TARGET_BOOT_ANIMATION_RES := 720
+WITH_GAPPS := false
+TARGET_GAPPS_ARCH := arm64
